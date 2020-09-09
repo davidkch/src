60cca837a798113016a9007c4be0f24b07102826
David Cho <yongkch@hotmail.com>
2020-09-09 13:06:53 -0700

    added python practice files

--- python/volume.py
+++ python/volume.py
@@ -4,8 +4,9 @@
 # There are libraries of common functions, for example a math library
 # that has functions such as sine, cosine, logs, etc.
 # There ARE syntax and semantic errors in this program as given!
-import pdb
-pdb.set_trace()
+#import pdb
+import math
+#pdb.set_trace()
 
 from math import pi   # use the value of pi defined in the math library
 def main():
@@ -13,7 +14,7 @@ def main():
       height = eval(input("Enter the height: "))
       conevol = 1/3*pi*radius*2*height   # cone volume
       cylvol = pi*radius**2*height        # cylinder volume
-      spherevol = 3/4*Pi*r**3        # sphere volume
+      spherevol = 3/4*pi*radius**3        # sphere volume
       print ('The volume of a cone: ',conevol)
       print ("The volume of a cylinder: ",cylvol)
       print ("The volume of a sphere: ",spherevol)
